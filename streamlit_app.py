ef8ceb395595c7d95524ffbb73afa5bf9d884e0f
igit-ng-pusa <cbdelumen@gmail.com>
2025-01-06 10:32:45 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,48 +9,7 @@ st.write(
     """
     Choose the fruits you want in your custom Smoothie.
     """)
-cnx = st.connection("snowflake")
-session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-#st.stop()
 
-pd_df=my_dataframe.to_pandas()
-#st.dataframe(pd_df)
-#st.stop()
-
-ingredients_list = st.multiselect(
-    'Choose up to 5 ingredients:'
-    , my_dataframe
-    , max_selections=5
-)
-if ingredients_list:
-
-    ingredients_string = ''
-    for fruit_chosen in ingredients_list :
-        ingredients_string += fruit_chosen+' '
-
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-        
-        st.subheader(fruit_chosen + 'Nutrition Information')
-        fruityvice_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
-        sf_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-    #st.write(ingredients_string)
-
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-            values ('""" + ingredients_string + """','"""+namem_on_order+""""')"""
-            # Import python packages
-import streamlit as st
-from snowflake.snowpark.functions import col
-import requests
-import pandas
-# Write directly to the app
-st.title(":cup_with_straw: Customize Your Smoothie:cup_with_straw:")
-st.write(
-    """
-    Choose the fruits you want in your custom Smoothie.
-    """)
 name_on_order = st.text_input("Name on Smoothie")
 st.write("Name on your smoothie will be", name_on_order)
 
@@ -84,7 +43,7 @@ if ingredients_list:
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-            values ('""" + ingredients_string + """','"""+ name_on_order +""""')"""
+            values ('""" + ingredients_string + """','"""+namem_on_order+""""')"""
 
     st.write(my_insert_stmt)
 
